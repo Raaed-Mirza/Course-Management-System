7c62a670f92d890d7a5a249d5e612467bc09580e
Raaed-Mirza <raaedmirza@gmail.com>
2025-05-17 09:45:11 -0400

    Added exams, quizzes, and tests files

--- src/main/java/com/coursemanager/Exams.java
+++ src/main/java/com/coursemanager/Exams.java
@@ -2,14 +2,14 @@ package com.coursemanager;
 
 import java.time.LocalDate;
 
-public class Assignment extends Tasks {
+public class Exams extends Tasks {
 
-    public Assignment(String title, LocalDate dueDate, double grade, double weight) {
+    public Exams(String title, LocalDate dueDate, double grade, double weight) {
         super(title, dueDate, grade, weight);
     }
 
     @Override
     public String getType() {
-        return "Assignment";
+        return "Exam";
     }
 }
