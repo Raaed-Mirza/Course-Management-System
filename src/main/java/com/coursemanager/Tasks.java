a4908681c37993691ec06ce976c2f7b5ac4c8c0d
Raaed-Mirza <raaedmirza@gmail.com>
2025-05-17 08:29:13 -0400

    Added Tasks base class for exams, assignments, tests, etc.

---
+++ src/main/java/com/coursemanager/Tasks.java
@@ -0,0 +1,52 @@
+package com.coursemanager;
+
+import java.time.LocalDate;
+
+public abstract class Tasks {
+
+    protected String title;
+    protected LocalDate dueDate;
+    protected double grade;
+    protected double weight;
+
+    public Tasks(String title, LocalDate dueDate, double grade, double weight) {
+        this.title = title;
+        this.dueDate = dueDate;
+        this.grade = grade;
+        this.weight = weight;
+    }
+
+    public double getGrade() {
+        return grade;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public LocalDate getDueDate() {
+        return dueDate;
+    }
+
+    public void setGrade(double grade) {
+        this.grade = grade;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setDueDate(LocalDate dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public abstract String getType();
+}
--- src/main/java/com/coursemanager/User.java
+++ src/main/java/com/coursemanager/User.java
@@ -4,11 +4,23 @@ public abstract class User {
 
     protected String name;
     protected String email;
+    protected String userId;
 
-    public User(String name, String email) {
+    public User(String name, String email, String userId) {
         this.name = name;
         this.email = email;
+        this.userId = userId;
     }
 
-    public abstract void printDashboard();
+    public String getUserId() {
+        return userId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
 }
